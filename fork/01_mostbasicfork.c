92b06687e2ead68843721ff34ae84a43c29ecfd5
K Kollmann <git@k.kollmann.moe>
2015-03-14 22:16:36 +0100

    add comments

--- fork/01_mostbasicfork.c
+++ fork/01_mostbasicfork.c
@@ -14,11 +14,15 @@ int main()
     child_pid = fork();
 
     printf("starting... \n");
-    printf("child pid: %i\n", child_pid);
+
+    // depending on the process we're in
+    // the output for child_pid is either 0 (for the child)
+    // or a number > 0 (for the parent)
+    printf("value of child_pid: %i\n", child_pid);
     sleep(2);
-    printf("child pid: %i\n", child_pid);
+    printf("value of child_pid: %i\n", child_pid);
     sleep(2);
-    printf("child pid: %i\n", child_pid);
+    printf("value of child_pid: %i\n", child_pid);
     printf("stopping... \n");
 
     return 0;
