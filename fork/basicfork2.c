2b4a37685f990856035fee42a0283ad3c22a238c
K Kollmann <git@k.kollmann.moe>
2015-03-14 22:00:14 +0100

    include/print out process ids

--- fork/basicfork2.c
+++ fork/basicfork2.c
@@ -13,21 +13,28 @@
 int main()
 {
     int child_pid;
+    child_pid = fork();
 
-    switch(child_pid = fork()) {
+    // parent process
+    if(child_pid > 0) {
+        sleep(4);
+        printf("this is the parent process\n");
+        printf("the parent's PID is: %d\n", getpid());
+        printf("its child's PID is: %d\n", child_pid);
+        sleep(2);
 
-        // error
-        case -1:
-            perror("something went wrong...\n");
-            exit(0);
+    // child process
+    } else if (child_pid == 0) {
+        sleep(2);
+        printf("this is the child process\n");
+        printf("the child's PID is: %d\n", getpid());
+        printf("its parent's PID is: %d\n", getppid());
+        sleep(2);
 
-        // child process
-        case 0:
-            printf("this is the child process\n");
-
-        // parent process
-        default:
-            printf("this is the parent process\n");
+    // error while forking
+    } else {
+        perror("An error occourred.");
+        exit(1);
     }
 
     return 0;
