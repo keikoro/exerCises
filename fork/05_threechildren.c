0aa82bfa413f1f63fe9d2f91434b4762fa266873
K Kollmann <git@k.kollmann.moe>
2015-03-15 00:15:00 +0100

    start program with 3 children

---
+++ fork/05_threechildren.c
@@ -0,0 +1,18 @@
+/*
+ *  threechildren.c
+ *
+ *  A "Hello, world" program with one parent and three child processes.
+ *  One child prints "hello", another "world", and the third ", ";
+ *  their parent waits for them, then finishes by printing "!" and a newline.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main()
+{
+
+    return 0;
+}
