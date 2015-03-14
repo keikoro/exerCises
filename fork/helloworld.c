d79bb4e7720f96fa5abcecc954905b17659110f5
K Kollmann <git@k.kollmann.moe>
2015-03-14 20:58:45 +0100

    begin helloworld program (forking, parent/child processes)

---
+++ fork/helloworld.c
@@ -0,0 +1,8 @@
+/*
+ *  helloworld.c
+ *
+ *  A program with two processes:
+ *  The child process prints "Hello", the parent process prints "world".
+ *  The parent waits for the child process to finish, then finishes with "\n".
+ */
+
