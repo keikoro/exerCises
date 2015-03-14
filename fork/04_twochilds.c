cd53920acaf882084cebfde537e46cf2d0a59a8c
K Kollmann <git@k.kollmann.moe>
2015-03-14 22:29:11 +0100

    start new program with two child processes

---
+++ fork/04_twochilds.c
@@ -0,0 +1,16 @@
+/*
+ *  twochilds.c
+ *
+ *  A "Hello, world" program with one parent and two child processes.
+ *  The first child prints "Hello, ", the second prints "world";
+ *  the parent of them both finishes the sentence by adding "!" and a newline.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+    return 0;
+}
