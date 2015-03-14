ece038e2a9e72220859f892cd6baa48a094618a1
K Kollmann <git@k.kollmann.moe>
2015-03-15 00:31:43 +0100

    rm else statements (debug prints), add sleeps -> proves children are out of order

--- fork/05_threechildren.c
+++ fork/05_threechildren.c
@@ -18,27 +18,23 @@ int main()
 
     if ((ch1 = fork()) == 0) {
         printf("Hello");
+        sleep(1);
         exit(EXIT_SUCCESS);
-    } else {
-        printf("parent, ch1 started\n");
     }
 
     if ((ch2 = fork()) == 0) {
         printf(", ");
+        sleep(1);
         exit(EXIT_SUCCESS);
-    } else {
-        printf("parent, ch2 started\n");
     }
 
     if ((ch3 = fork()) == 0) {
         printf("world");
+        sleep(1);
         exit(EXIT_SUCCESS);
-    } else {
-        printf("parent, ch3 started\n");
     }
 
     while (wait(&status) > 0) {
-        printf("successful exits\n");
     }
 
     printf("!\n");
