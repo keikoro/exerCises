49c6ebc80302453a6bb58ad5ab465728206fb2ab
K Kollmann <git@k.kollmann.moe>
2015-03-15 01:46:00 +0100

    switch children/parent execution around for all

--- fork/05_threechildren.c
+++ fork/05_threechildren.c
@@ -2,8 +2,11 @@
  *  threechildren.c
  *
  *  A "Hello, world" program with one parent and three child processes.
- *  One child prints "hello", another "world", and the third ", ";
+ *  One child prints "Hello", another "world", and the third ", ";
  *  their parent waits for them, then finishes by printing "!" and a newline.
+ *
+ *  Since processes run in parallel, it's not the point of this program
+ *  to print out "Hello, world" in the correct order.
  */
 
 #include <stdio.h>
@@ -13,29 +16,41 @@
 
 int main()
 {
-    pid_t ch1, ch2, ch3, waitpid;
+    pid_t ch1, ch2, ch3, wpid;
     int status = 0;
+    int a[3] = {0};
 
-    if ((ch1 = fork()) == 0) {
+    if ((ch1 = fork()) > 0) {
+        a[0] = ch1;
+    } else if (ch1 == 0) {
         printf("Hello");
         sleep(1);
         exit(EXIT_SUCCESS);
     }
 
-    if ((ch2 = fork()) == 0) {
+    if ((ch2 = fork()) > 0) {
+        a[1] = ch2;
+    } else if (ch2 == 0) {
         printf(", ");
         sleep(1);
         exit(EXIT_SUCCESS);
     }
 
-    if ((ch3 = fork()) == 0) {
+    if ((ch3 = fork()) > 0) {
+        a[2] = ch3;
+    } else if (ch3 == 0) {
         printf("world");
         sleep(1);
         exit(EXIT_SUCCESS);
     }
 
-    while (wait(&status) > 0) {
-        // wait for all children processes to be finished
+    // wait for all child processes to be finished
+    // only then print the exclamation mark and newline
+    while ( (wpid = waitpid(a[0], &status, 0)) == 0) {
+    }
+    while ( (wpid = waitpid(a[1], &status, 0)) == 0) {
+    }
+    while ( (wpid = waitpid(a[2], &status, 0)) == 0) {
     }
 
     printf("!\n");
