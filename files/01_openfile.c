8b216366b6ce8f703363b6e9585132e243005983
K Kollmann <git@k.kollmann.moe>
2015-04-10 16:52:18 +0200

    add program for opening files

--- files/01_openfile.c
+++ files/01_openfile.c
@@ -5,9 +5,21 @@
  */
 
  #include <stdio.h>
+ #include <stdlib.h>
 
  int main() {
 
-    return 0;
+    FILE *input;
+    char filename[] = "demo.txt";
+    char *inputfile = (char*)malloc(256*sizeof(char));
+    inputfile = &filename[0];
 
+    if ((input = fopen(inputfile, "r")) != NULL) {
+        printf("Success!\n");
+    } else {
+        fprintf(stderr, "Cannot open input file %s\n", inputfile);
+        exit(1);
+    }
+
+    return 0;
  }
