1947def89b7dde5d818b6f01355b16da205aa754
K Kollmann <git@k.kollmann.moe>
2015-04-10 17:32:27 +0200

    rm newline in filename caused by fgets

--- files/02_opendifferentfiles.c
+++ files/02_opendifferentfiles.c
@@ -1,11 +1,12 @@
 /*
-*  opendifferentfiles.c
-*
-*  A program for opening user-provided files.
-*/
+ *  opendifferentfiles.c
+ *
+ *  A program for opening user-provided files.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAXCHARS 255
 
 int main() {
@@ -13,8 +14,15 @@ int main() {
     FILE *input;
     char *inputfile = (char*)malloc(256*sizeof(char));
 
-    printf("Please provide the name of a file to be opened:\n");
+    printf("Please provide the name of/path to a file to be opened:\n");
     fgets(inputfile, MAXCHARS, stdin);
+
+    // get rid of newline in inputfile (caused by fgets)
+    // by replacing it with null terminator
+    size_t flength = strlen(inputfile);
+    if (inputfile[flength - 1] == '\n') {
+        inputfile[flength - 1] = '\0';
+    }
 
     if ((input = fopen(inputfile, "r")) != NULL) {
         printf("Success, file %s exists!\n", inputfile);
