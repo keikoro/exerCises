c14bdcd33100a3ddc8c2e174a81dc12ff0165122
K Kollmann <git@k.kollmann.moe>
2015-04-12 20:22:46 +0200

    add check for existence of file

--- files/06_deletefile.c
+++ files/06_deletefile.c
@@ -7,15 +7,26 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 
-    // FILE *deleteme;
-    // char *filename;
+    FILE *deleteme;
+    char *filename;
 
     // print usage message if not enough arguments were provided
     if (argc < 2) {
         printf("Usage: deletefile file2delete\n");
+    } else {
+        // check that file exists
+        filename = argv[1];
+
+        if ((deleteme = fopen(filename, "r")) == NULL) {
+            fprintf(stderr, "File to delete does not exist! Please try again.\n");
+            exit(1);
+        } else {
+        }
+
     }
 
     return 0;
