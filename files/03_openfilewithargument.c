18b4a90522c580f8102f933f2f0b489682958f9d
K Kollmann <git@k.kollmann.moe>
2015-04-12 02:25:48 +0200

    finished program

--- files/03_openfilewithargument.c
+++ files/03_openfilewithargument.c
@@ -6,8 +6,29 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#define MAXLENGTH 255
 
-int main()
+int main(int argc, char *argv[])
 {
+    FILE *f;
+
+    // check if there are enough command line arguments
+    // and print out usage message if necessary
+    if (argc != 2) {
+        printf("Usage: openfilewithargument filename\n");
+    } else {
+        char *inputfile = (char *) malloc(MAXLENGTH * sizeof(char));
+        inputfile = argv[1];
+
+        if ((f = fopen(inputfile, "r")) != NULL) {
+            printf("Success! File %s exists!\n", inputfile);
+        } else {
+            fprintf(stderr, "Cannot open input file %s\n", inputfile);
+        }
+
+
+    }
+
     return 0;
 }
