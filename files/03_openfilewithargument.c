f2898723d1fea90636c85d6e5bbcf45ce8a311e1
K Kollmann <git@k.kollmann.moe>
2015-04-12 02:15:37 +0200

    new program to open files with cli argument

---
+++ files/03_openfilewithargument.c
@@ -0,0 +1,13 @@
+/*
+ *  openfilewithargument.c
+ *
+ *  A program demonstrating opening a file from the command line
+ *  by providing the file name as an argument.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
